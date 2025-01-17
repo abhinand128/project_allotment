4e4dbdebd7e5213f8a39fa3b723a04fc456a5b38
abhinand128 <abhinanda128@gmail.com>
2025-01-17 14:46:31 +0530

    created_views

--- path/admin_panel/views.py
+++ path/admin_panel/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from student.models import Student, CourseAllotment
 
-# Create your views here.
+def admin_dashboard(request):
+    # View students, courses, and manage deadlines
+    students = Student.objects.all()
+    return render(request, 'admin_panel/dashboard.html', {'students': students})
+
+
+def allocate_courses(request):
+    # Allocation logic based on normalized marks
+    return render(request, 'admin_panel/allocate_courses.html')
